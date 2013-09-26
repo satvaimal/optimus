bf666e50d40c3f43a7f4bd7fa9e33dda92b34b37
Alejandro García <alejandro.garcia.granados@gmail.com>
2013-09-26 13:57:42 -0500

    Plugin info updated

--- OptimusGrailsPlugin.groovy
+++ OptimusGrailsPlugin.groovy
@@ -17,12 +17,12 @@ Grails Optimus Plugin
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/optimus"
+    def documentation = "http://satvaimal.github.io/optimus/"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
@@ -34,7 +34,7 @@ Grails Optimus Plugin
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/satvaimal/optimus/tree/master" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
