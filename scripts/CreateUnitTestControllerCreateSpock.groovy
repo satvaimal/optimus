c3a03597c758ff9304c07e8f2d62fb3943fd7687
Alejandro García <alejandro.garcia.granados@gmail.com>
2013-10-07 19:37:50 -0500

    Added Spock unit test command for 'create' controller method

---
+++ scripts/CreateUnitTestControllerCreateSpock.groovy
@@ -0,0 +1,103 @@
+import org.apache.commons.lang.WordUtils
+
+includeTargets << new File( optimusPluginDir,
+    'scripts/_OptimusUtils.groovy' )
+
+target( createUnitTestControllerCreateSpock:
+    "Generate Spock unit tests for 'create' controller method" ) {
+
+    depends( checkVersion, configureProxy, packageApp, loadApp, configureApp )
+    def domainClassList = getDomainClassList( args )
+    if ( !domainClassList ) return
+    domainClassList.each { generate( it ) }
+    def msg = "Finished generation of Spock 'create' controller unit tests"
+    event( 'StatusFinal', [ msg ] )
+
+}// End of closure
+
+setDefaultTarget( createUnitTestControllerCreateSpock )
+
+void generate( domainClass ) {
+
+    def content = '' << "package ${domainClass.packageName}\n\n"
+    content << generateImports()
+    content << generateClassDeclaration( domainClass.name )
+    content << generateSetUpMethod( domainClass.name )
+    content << generateOkMethod( domainClass.name )
+    content << generateRequestMethodInvalidMethod()
+    content << generateGetTemplateMethod( domainClass.name )
+    content << '}'
+    def directory = generateDirectory( "test/unit", domainClass.packageName )
+    def fileName = "${domainClass.name}ControllerCreateSpec.groovy"
+    new File(directory, fileName).text = content.toString()
+
+}// End of method
+
+String generateImports() {
+
+    def content = '' << "import grails.test.mixin.*\n"
+    content << "import spock.lang.*\n\n"
+    content.toString()
+
+}// End of method
+
+String generateClassDeclaration( className ) {
+
+    def content = '' << "@TestFor(${className}Controller)\n"
+    content << "class ${className}ControllerCreateSpec"
+    content << " extends Specification {\n\n"
+    content.toString()
+
+}// End of method
+
+String generateSetUpMethod( className ) {
+
+    def classNameLower = WordUtils.uncapitalize( className )
+    def content = '' << "${TAB}def setup() {\n"
+    content << "${TAB*2}views[ '/${classNameLower}/_form.gsp' ]"
+    content << " = this.getTemplate()\n"
+    content << "${TAB}}\n\n"
+    content.toString()
+
+}// End of method
+
+String generateOkMethod( className ) {
+
+    def classNameLower = WordUtils.uncapitalize( className )
+    def content = '' << "${TAB}def \"test ok\"() {\n\n"
+    content << "${TAB*2}when:\n"
+    content << "${TAB*3}request.method = 'GET'\n"
+    content << "${TAB*3}def model = controller.create()\n"
+    content << "${TAB*2}then:\n"
+    content << "${TAB*3}response.text == 'OK'\n"
+    content << "${TAB*3}response.status == 200\n\n"
+    content << "${TAB}}\n\n"
+    content.toString()
+
+}// End of method
+
+String generateRequestMethodInvalidMethod() {
+
+    def content = '' << "${TAB}@Ignore( 'See http://jira.grails.org/browse/"
+    content << "GRAILS-8426' )\n"
+    content << "${TAB}def \"test request method invalid\"() {\n\n"
+    content << "${TAB*2}when:\n"
+    content << "${TAB*3}request.method = 'POST'\n"
+    content << "${TAB*3}controller.create()\n"
+    content << "${TAB*2}then:\n"
+    content << "${TAB*3}response.status == 405\n\n"
+    content << "${TAB}}\n\n"
+    content.toString()
+
+}// End of method
+
+String generateGetTemplateMethod( className ) {
+
+    def classNameLower = WordUtils.uncapitalize( className )
+    def content = '' << "${TAB}private String getTemplate() {\n"
+    content << "${TAB*2}'<g:if test=\"\${${classNameLower}Instance}\">OK</g:if>"
+    content << "<g:else>ERROR</g:else>'\n"
+    content << "${TAB}}\n\n"
+    content.toString()
+
+}// End of method
